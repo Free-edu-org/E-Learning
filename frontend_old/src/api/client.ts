a281ea987b50e038e7c47089d109d50205eebc8a
Dominik Ludwinski <ludwinskidominik02@gmail.com>
2026-01-24 17:37:21 +0100

    clean up

--- frontend_old/src/api/client.ts
+++
@@ -1,9 +0,0 @@
-export async function apiClient<T>(input: RequestInfo, init?: RequestInit): Promise<T> {
-  const response = await fetch(input, init);
-
-  if (!response.ok) {
-    throw new Error(`API request failed: ${response.status}`);
-  }
-
-  return response.json() as Promise<T>;
-}
